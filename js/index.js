76ce893c2e04227548e29a10fc0afee6fecfa428
efyzhi <pelurobert10@gmail.com>
2023-06-05 02:22:24 +0100

    adjust

--- js/index.js
+++ js/index.js
@@ -13,3 +13,36 @@ function setActiveLink(event, element) {
 
     element.classList.add('active');
 }
+
+const themeToggle = document.querySelector('#theme-toggle');
+const body = document.querySelector('body');
+
+themeToggle.addEventListener('click', () => {
+    body.classList.toggle('dark-theme');
+})
+
+window.addEventListener('scroll', function() {
+    let header = document.querySelector('header');
+    let homeSection = document.getElementById('home');
+    let headerHeight = header.offsetHeight;
+
+    let homeSectionBottom = homeSection.offsetTop + homeSection.offsetHeight;
+
+    if (window.pageYOffset > homeSectionBottom - headerHeight) {
+        header.classList.add('header-scrolled');
+    } else {
+        header.classList.remove('header-scrolled')
+    }
+})
+
+document.getElementById("menu-toggle").addEventListener("click", function() {
+    let mobileMenuSidebar = document.querySelector(".mobile-menu-sidebar");
+    mobileMenuSidebar.classList.toggle("active");  
+  });
+  
+  document.querySelectorAll(".mobile-menu li a").forEach(function(link) {
+    link.addEventListener("click", function() {
+      let mobileMenuSidebar = document.querySelector(".mobile-menu-sidebar");
+      mobileMenuSidebar.classList.remove("active");
+    });
+  });
